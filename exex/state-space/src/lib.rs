ba15601dec53d8619e26579b1a4ed34395366a01
0xKitsune <0xkitsune@protonmail.com>
2024-08-21 01:02:22 -0400

    update state change cache

--- exex/state-space/src/lib.rs
+++ exex/state-space/src/lib.rs
@@ -1,17 +1,13 @@
 use amms::{
     amm::{AutomatedMarketMaker, AMM},
-    state_space::error::StateChangeError,
+    state_space::{error::StateChangeError, StateChange},
 };
 
-use alloy::{
-    primitives::{Address, B256},
-    rpc::types::eth::Filter,
-};
+use alloy::primitives::Address;
 
 use amms::state_space::{StateChangeCache, StateSpace};
 use arraydeque::ArrayDeque;
 use reth_exex::ExExNotification;
-use reth_node_api::FullNodeComponents;
 use reth_primitives::Log;
 use reth_provider::ExecutionOutcome;
 use std::{
@@ -19,13 +15,16 @@ use std::{
     sync::Arc,
 };
 use tokio::sync::RwLock;
+
 #[derive(Debug)]
 pub struct StateSpaceManagerExEx {
     state: Arc<RwLock<StateSpace>>,
+    _latest_synced_block: u64,
+    state_change_cache: Arc<RwLock<StateChangeCache>>,
 }
 
 impl StateSpaceManagerExEx {
-    pub fn new(amms: Vec<AMM>) -> Self {
+    pub fn new(amms: Vec<AMM>, latest_synced_block: u64) -> Self {
         let state: HashMap<Address, AMM> = amms
             .into_iter()
             .map(|amm| (amm.address(), amm))
@@ -33,6 +32,8 @@ impl StateSpaceManagerExEx {
 
         Self {
             state: Arc::new(RwLock::new(state)),
+            _latest_synced_block: latest_synced_block,
+            state_change_cache: Arc::new(RwLock::new(ArrayDeque::new())),
         }
     }
 
@@ -40,20 +41,32 @@ impl StateSpaceManagerExEx {
         &self,
         notification: ExExNotification,
     ) -> Result<Vec<Address>, StateChangeError> {
+        // TODO: return addresses affected by state changes
         match notification {
             ExExNotification::ChainCommitted { new } => {
-                let execuiton_outcome = new.execution_outcome();
-                self.handle_state_changes(execuiton_outcome).await
+                self.handle_state_changes(new.execution_outcome()).await
             }
 
             ExExNotification::ChainReorged { old: _old, new } => {
-                let execuiton_outcome = new.execution_outcome();
-                self.handle_state_changes(execuiton_outcome).await
+                self.handle_reorgs(new.execution_outcome()).await
             }
             ExExNotification::ChainReverted { old: _old } => {
                 unimplemented!("handle chain reverts");
             }
         }
+    }
+
+    pub async fn handle_reorgs(
+        &self,
+        new: &ExecutionOutcome,
+    ) -> Result<Vec<Address>, StateChangeError> {
+        // Unwind the state changes from the old state to the new state
+
+        // TODO: should this be - 1?
+        self.unwind_state_changes(new.first_block - 1).await?;
+
+        // Apply the new state changes
+        self.handle_state_changes(new).await
     }
 
     pub async fn handle_state_changes(
@@ -77,9 +90,12 @@ impl StateSpaceManagerExEx {
         logs: Vec<Log>,
     ) -> Result<Vec<Address>, StateChangeError> {
         let mut updated_amms = HashSet::new();
+
+        let mut state = self.state.write().await;
+        let mut state_change_cache = self.state_change_cache.write().await;
         for log in logs.into_iter() {
             // check if the log is from an amm in the state space
-            if let Some(amm) = self.state.write().await.get_mut(&log.address) {
+            if let Some(amm) = state.get_mut(&log.address) {
                 if !updated_amms.contains(&log.address) {
                     updated_amms.insert(log.address);
                 }
@@ -90,9 +106,63 @@ impl StateSpaceManagerExEx {
                 };
 
                 amm.sync_from_log(rpc_log)?;
+
+                // TODO:
+                // self.add_state_change_to_cache(*state_change_cache, StateChange {});
             }
         }
 
         Ok(updated_amms.into_iter().collect())
+    }
+
+    fn add_state_change_to_cache<const CAP: usize>(
+        &self,
+        mut state_change_cache: ArrayDeque<StateChange, CAP>,
+        state_change: StateChange,
+    ) -> Result<(), StateChangeError> {
+        if state_change_cache.is_full() {
+            state_change_cache.pop_back();
+            state_change_cache
+                .push_front(state_change)
+                .map_err(|_| StateChangeError::CapacityError)?
+        } else {
+            state_change_cache
+                .push_front(state_change)
+                .map_err(|_| StateChangeError::CapacityError)?
+        }
+        Ok(())
+    }
+
+    /// Unwinds the state changes cache for every block from the most recent state change cache back to the block to unwind -1.
+    async fn unwind_state_changes(&self, block_to_unwind: u64) -> Result<(), StateChangeError> {
+        let mut state_change_cache = self.state_change_cache.write().await;
+
+        // TODO: We can write the state change cache more efficiently
+        loop {
+            // Acquire a lock on state while unwinding state changes
+            let mut state = self.state.write().await;
+            // check if the most recent state change block is >= the block to unwind,
+            if let Some(state_change) = state_change_cache.get(0) {
+                if state_change.block_number >= block_to_unwind {
+                    if let Some(option_state_changes) = state_change_cache.pop_front() {
+                        if let Some(state_changes) = option_state_changes.state_change {
+                            for amm_state in state_changes {
+                                state.insert(amm_state.address(), amm_state);
+                            }
+                        }
+                    } else {
+                        // We know that there is a state change from state_change_cache.get(0) so when we pop front without returning a value, there is an issue
+                        return Err(StateChangeError::PopFrontError);
+                    }
+                } else {
+                    return Ok(());
+                }
+            } else {
+                // We return an error here because we never want to be unwinding past where we have state changes.
+                // For example, if you initialize a state space that syncs to block 100, then immediately after there is a chain reorg to 95, we can not roll back the state
+                // changes for an accurate state space. In this case, we return an error
+                return Err(StateChangeError::NoStateChangesInCache);
+            }
+        }
     }
 }
