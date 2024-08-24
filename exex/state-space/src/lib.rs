c82ec3d7c06d558e129060b05bafd5f70a68e7cf
0xKitsune <0xkitsune@protonmail.com>
2024-08-24 01:09:24 -0400

    added todos

--- exex/state-space/src/lib.rs
+++ exex/state-space/src/lib.rs
@@ -1,20 +1,36 @@
 use amms::{
     amm::{AutomatedMarketMaker, AMM},
-    state_space::{error::StateChangeError, StateChange},
+    state_space::error::StateChangeError,
 };
 
-use alloy::primitives::Address;
+use alloy::{primitives::Address, signers::k256::elliptic_curve::rand_core::block};
 
 use amms::state_space::{StateChangeCache, StateSpace};
 use arraydeque::ArrayDeque;
 use reth_exex::ExExNotification;
 use reth_primitives::Log;
-use reth_provider::ExecutionOutcome;
+use reth_provider::{ExecutionDataProvider, ExecutionOutcome};
 use std::{
     collections::{HashMap, HashSet},
     sync::Arc,
 };
 use tokio::sync::RwLock;
+
+// TODO: Implement this directly into amms-rs, removing the option from state change
+#[derive(Debug)]
+pub struct StateChange {
+    pub state_change: Vec<AMM>,
+    pub block_number: u64,
+}
+
+impl StateChange {
+    pub fn new(state_change: Vec<AMM>, block_number: u64) -> Self {
+        Self {
+            block_number,
+            state_change,
+        }
+    }
+}
 
 #[derive(Debug)]
 pub struct StateSpaceManagerExEx {
@@ -41,15 +57,14 @@ impl StateSpaceManagerExEx {
         &self,
         notification: ExExNotification,
     ) -> Result<Vec<Address>, StateChangeError> {
-        // TODO: return addresses affected by state changes
         match notification {
             ExExNotification::ChainCommitted { new } => {
-                self.handle_state_changes(new.execution_outcome()).await
+                let num_blocks = new.blocks().len() as u64;
+                self.handle_state_changes(new.execution_outcome(), num_blocks)
+                    .await
             }
 
-            ExExNotification::ChainReorged { old: _old, new } => {
-                self.handle_reorgs(new.execution_outcome()).await
-            }
+            ExExNotification::ChainReorged { old: _old, new } => self.handle_reorgs(new).await,
             ExExNotification::ChainReverted { old: _old } => {
                 unimplemented!("handle chain reverts");
             }
@@ -58,41 +73,51 @@ impl StateSpaceManagerExEx {
 
     pub async fn handle_reorgs(
         &self,
-        new: &ExecutionOutcome,
+        new: Arc<reth_execution_types::Chain>,
     ) -> Result<Vec<Address>, StateChangeError> {
         // Unwind the state changes from the old state to the new state
-
+        let first_block = new.first().number;
         // TODO: should this be - 1?
-        self.unwind_state_changes(new.first_block - 1).await?;
+        self.unwind_state_changes(first_block - 1).await?;
 
         // Apply the new state changes
-        self.handle_state_changes(new).await
+        let num_blocks = new.blocks().len() as u64;
+
+        self.handle_state_changes(new.execution_outcome(), num_blocks)
+            .await
     }
 
     pub async fn handle_state_changes(
         &self,
         execution_outcome: &ExecutionOutcome,
+        num_blocks: u64,
     ) -> Result<Vec<Address>, StateChangeError> {
-        let block_number = execution_outcome.first_block();
+        let first_block = execution_outcome.first_block();
 
-        let logs = (block_number
-            ..=(block_number + execution_outcome.receipts().receipt_vec.len() as u64 - 1))
-            .filter_map(|block_number| execution_outcome.logs(block_number))
-            .flatten()
-            .cloned()
-            .collect::<Vec<Log>>();
+        let mut aggregated_updates = vec![];
+        for block_number in first_block..first_block + num_blocks {
+            if let Some(logs) = execution_outcome.logs(block_number) {
+                let updated_amms = self.modify_state_from_logs(logs, block_number).await?;
+                aggregated_updates.extend_from_slice(&updated_amms);
+            }
+        }
 
-        self.modify_state_from_logs(logs).await
+        Ok(aggregated_updates)
     }
 
     async fn modify_state_from_logs(
         &self,
-        logs: Vec<Log>,
+        logs: impl Iterator<Item = &Log>,
+        block_number: u64,
     ) -> Result<Vec<Address>, StateChangeError> {
         let mut updated_amms = HashSet::new();
 
         let mut state = self.state.write().await;
-        let mut state_change_cache = self.state_change_cache.write().await;
+
+        // NOTE: we need to group the logs by block number before calling this function but we should assume that all logs are
+        let mut state_changes = vec![];
+
+        // TODO: collect state changes for the block and then add them
         for log in logs.into_iter() {
             // check if the log is from an amm in the state space
             if let Some(amm) = state.get_mut(&log.address) {
@@ -105,16 +130,24 @@ impl StateSpaceManagerExEx {
                     ..Default::default()
                 };
 
+                state_changes.push(amm.clone());
                 amm.sync_from_log(rpc_log)?;
-
-                // TODO:
-                // self.add_state_change_to_cache(*state_change_cache, StateChange {});
             }
+        }
+
+        if !state_changes.is_empty() {
+            let mut state_change_cache = self.state_change_cache.write().await;
+
+            self.add_state_change_to_cache(
+                *state_change_cache,
+                StateChange::new(state_changes, block_number),
+            )?;
         }
 
         Ok(updated_amms.into_iter().collect())
     }
 
+    // TODO: implement this directly as trait method on StateChangeCache
     fn add_state_change_to_cache<const CAP: usize>(
         &self,
         mut state_change_cache: ArrayDeque<StateChange, CAP>,
@@ -134,6 +167,8 @@ impl StateSpaceManagerExEx {
     }
 
     /// Unwinds the state changes cache for every block from the most recent state change cache back to the block to unwind -1.
+
+    // TODO: implement this directly into amms-rs. Additonally this can be more efficient, only storing and unwinding up to block_to_unwind rather than unwinding blocks with None
     async fn unwind_state_changes(&self, block_to_unwind: u64) -> Result<(), StateChangeError> {
         let mut state_change_cache = self.state_change_cache.write().await;
 
