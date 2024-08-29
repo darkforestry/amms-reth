894a895bcd16e290faeac000a502438c94ebcd9a
0xKitsune <0xkitsune@protonmail.com>
2024-08-29 00:42:05 -0400

    implement logic to handle reorgs and chain reverts

--- exex/state-space/src/lib.rs
+++ exex/state-space/src/lib.rs
@@ -36,28 +36,29 @@ impl StateSpaceManagerExEx {
                     .await
             }
 
-            ExExNotification::ChainReorged { old: _old, new } => self.handle_reorgs(new).await,
-            ExExNotification::ChainReverted { old: _old } => {
-                unimplemented!("handle chain reverts");
+            ExExNotification::ChainReorged { old: _, new } => {
+                let first_block = new.first().number;
+                let num_blocks = new.blocks().len() as u64;
+
+                self.state_change_cache
+                    .unwind_state_changes(first_block - 1);
+
+                self.handle_state_changes(new.execution_outcome(), num_blocks)
+                    .await
+            }
+            ExExNotification::ChainReverted { old } => {
+                let first_block = old.first().number;
+                let num_blocks = old.blocks().len() as u64;
+
+                let amms = self
+                    .state_change_cache
+                    .unwind_state_changes(first_block + num_blocks - 1);
+
+                let addresses = amms.iter().map(|amm| amm.address()).collect();
+
+                Ok(addresses)
             }
         }
-    }
-
-    pub async fn handle_reorgs(
-        &mut self,
-        new: Arc<reth_execution_types::Chain>,
-    ) -> Result<Vec<Address>, StateSpaceError> {
-        // Unwind the state changes from the old state to the new state
-        let first_block = new.first().number;
-
-        self.state_change_cache
-            .unwind_state_changes(first_block - 1);
-
-        // Apply the new state changes
-        let num_blocks = new.blocks().len() as u64;
-
-        self.handle_state_changes(new.execution_outcome(), num_blocks)
-            .await
     }
 
     pub async fn handle_state_changes(
